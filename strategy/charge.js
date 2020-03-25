55bdca3e1797117a7785500253a1aa0011d97e8c
Brian Levinstein <blevinstein@gmail.com>
2020-03-25 01:00:15 -0700

    Avoid charging past endzone

--- strategy/charge.js
+++ strategy/charge.js
@@ -9,7 +9,13 @@ export class ChargeStrategy extends Strategy {
 
   update() {
     for (let player of this.team.players) {
-      player.move(mul2d(getVector(this.team.goalDirection), 10));
+      if (this.team.goalDirection === 'E' && player.position[0] >= 90) {
+        player.rest(mul2d(getVector(this.team.goalDirection), -1));
+      } else if (this.team.goalDirection === 'W' && player.position[0] <= 20) {
+        player.rest(mul2d(getVector(this.team.goalDirection), -1));
+      } else {
+        player.move(mul2d(getVector(this.team.goalDirection), 10));
+      }
     }
     return true;
   }
