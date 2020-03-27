aa55af7c94de4e15ec2508238df7c672abaf6dfe
Brian Levinstein <blevinstein@gmail.com>
2020-03-26 20:51:49 -0700

    Cleanup error-prone variable reuse

--- strategy/closest_pickup.js
+++ strategy/closest_pickup.js
@@ -12,23 +12,23 @@ export class ClosestPickupStrategy extends Strategy {
     if (!this.game.disc.isLoose() && !this.team.onOffense) { return true; }
 
     if (this.game.disc.isLoose()) {
-      const target = this.game.disc.grounded
+      const discTarget = this.game.disc.grounded
           ? this.game.disc.position
           : Disc.simulateUntilGrounded(
                 sub3d(this.game.disc.position, [0, 0, HAND_HEIGHT]),
                 this.game.disc.velocity,
                 this.game.disc.upVector)[0];
 
-      const [closestPlayer] = Game.getClosestPlayer(this.team.players, target);
+      const [closestPlayer] = Game.getClosestPlayer(this.team.players, discTarget);
 
       for (let player of this.team.players) {
         if (player == closestPlayer) {
-          player.move(sub2d(target, player.position));
+          player.move(sub2d(discTarget, player.position));
         } else {
-          let target = Game.snapToBounds(
+          const moveTarget = Game.snapToBounds(
               add2d(player.position, mul2d(getVector(this.team.goalDirection), 10)),
               FIELD_BOUNDS);
-          player.move(sub2d(target, player.position));
+          player.move(sub2d(moveTarget, player.position));
         }
       }
     } else {
@@ -36,8 +36,8 @@ export class ClosestPickupStrategy extends Strategy {
       if (Game.boundsCheck(playerWithDisc.position, FIELD_BOUNDS_NO_ENDZONES)) {
         return true;
       } else {
-        let target = Game.snapToBounds(playerWithDisc.position, FIELD_BOUNDS_NO_ENDZONES);
-        playerWithDisc.move(sub2d(target, playerWithDisc.position));
+        const moveTarget = Game.snapToBounds(playerWithDisc.position, FIELD_BOUNDS_NO_ENDZONES);
+        playerWithDisc.move(sub2d(moveTarget, playerWithDisc.position));
       }
     }
   }
