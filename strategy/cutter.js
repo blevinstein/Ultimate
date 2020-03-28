c59307c83f8a6a9ba5458592dfdb2d3d0ad50a80
Brian Levinstein <blevinstein@gmail.com>
2020-03-28 04:57:37 -0700

    tweak

--- strategy/cutter.js
+++ strategy/cutter.js
@@ -6,7 +6,7 @@ export class Cutter {
   // Choose the best from a sample of random locations no more than 5 yards
   // behind the thrower. This is potentially an appropriate location for a
   // cutter to run to.
-  static chooseBestRandomDestination(game, team, numCandidateRoutes = 10) {
+  static chooseBestRandomDestination(game, team, numCandidateRoutes = 5) {
     const thrower = game.playerWithDisc();
     if (!thrower) { return null; }
     const defensivePlayers = game.defensiveTeam().players;
