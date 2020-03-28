b9423c69d253cba0dbb291080b3eccba80bcc343
Brian Levinstein <blevinstein@gmail.com>
2020-03-28 05:58:50 -0700

    Split into pages for Auto and Play

---
+++ play.js
@@ -0,0 +1,46 @@
+
+import { installMathUtils } from './math_utils.js';
+
+import { Coach } from './coach.js';
+import { Disc } from './disc.js';
+import { Game } from './game.js';
+import { Team } from './team.js';
+import { MAX_THROW_SPEED } from './player.js';
+import { RangeFinderFactory } from './range_finder.js';
+import { ManualOffenseStrategy } from './strategy/manual_offense.js';
+
+let initialized = false;
+
+window.initialize = () => {
+  console.log('Initializing...');
+
+  window.rangeFinder = RangeFinderFactory.create(MAX_THROW_SPEED);
+  installMathUtils(window);
+
+  Game.loadResources().then((resources) => {
+    initialized = true;
+    console.log('Initialized.');
+    start(resources);
+  }, (error) => {
+    console.log('Failed to initialize.');
+    console.log(error);
+  });
+
+  window.onkeypress = (event) => {
+    if (event.key.toUpperCase() === 'R') {
+      window.game.reset();
+      if (!window.game.isRunning()) { window.game.start(); }
+    } else if (event.key.toUpperCase() === 'Q') {
+      if (window.game.isRunning()) { window.game.stop(); }
+    } else if (event.key.toUpperCase() === 'W') {
+      if (!window.game.isRunning()) { window.game.start(); }
+    }
+  };
+}
+
+function start(resources) {
+  window.game = new Game(resources,
+      document.getElementById('canvas'),
+      [new Coach(), new Coach((game, team) => new ManualOffenseStrategy(game, team))]);
+  window.game.start();
+}
