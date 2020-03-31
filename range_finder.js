5336db163d290cd2abeb061a092f2e43f507bdd8
Brian Levinstein <blevinstein@gmail.com>
2020-03-30 22:02:21 -0700

    Fix RangeFinder when tiltAngle != 0

--- range_finder.js
+++ range_finder.js
@@ -1,9 +1,9 @@
 
-import { mag2d, mul2d } from './math_utils.js';
+import { mag2d, mul2d, rotate2d } from './math_utils.js';
 import { Disc } from './disc.js';
 
-const VELOCITY_STEP = 0.1;
-const ANGLE_STEP = 0.1;
+const VELOCITY_STEP = 0.05;
+const ANGLE_STEP = 0.05;
 
 const MAX_LAUNCH_ANGLE = 0.5;
 const MIN_LAUNCH_ANGLE = -0.2;
@@ -11,8 +11,8 @@ const MIN_LAUNCH_ANGLE = -0.2;
 const MIN_ANGLE_OF_ATTACK = -0.5;
 const MAX_ANGLE_OF_ATTACK = 0.4;
 
-const MIN_TILT = 0.0;
-const MAX_TILT = 0.0;
+const MIN_TILT = -1;
+const MAX_TILT = 1;
 
 export class RangeFinderFactory {
   static create(maxSpeed) {
@@ -37,40 +37,45 @@ export class RangeFinder {
       for (let speed = VELOCITY_STEP; speed <= maxSpeed; speed += VELOCITY_STEP) {
         for (let angleOfAttack = MIN_ANGLE_OF_ATTACK; angleOfAttack <= MAX_ANGLE_OF_ATTACK; angleOfAttack += ANGLE_STEP) {
           for (let tiltAngle = MIN_TILT; tiltAngle <= MAX_TILT; tiltAngle += ANGLE_STEP) {
-            let velocity = [speed * Math.cos(launchAngle), 0, speed * Math.sin(launchAngle)];
-            this.samples.push({
-              input: [velocity[0], velocity[2], angleOfAttack, tiltAngle],
-              output: Disc.simulateUntilGrounded(
-                  [0, 0, 0.1],
+            const velocity = [speed * Math.cos(launchAngle), 0, speed * Math.sin(launchAngle)];
+            const [groundedLocation, groundedTime] = Disc.simulateUntilGrounded(
+                  [0, 0, 0.01],
                   velocity,
-                  Disc.createUpVector(velocity, angleOfAttack, tiltAngle)),
+                  Disc.createUpVector(velocity, angleOfAttack, tiltAngle));
+            const groundedAngle = Math.atan2(groundedLocation[1], groundedLocation[0]);
+            const rotatedGrounded = rotate2d([groundedLocation[0], groundedLocation[1]], -groundedAngle).concat(0);
+            const rotatedVelocity = rotate2d(velocity, -groundedAngle).concat(velocity[2]);
+            if (Math.abs(rotatedGrounded[1]) > 0.001) { throw new Error('Failed to rotate point onto x-axis!'); }
+            this.samples.push({
+              input: {velocity: rotatedVelocity, angleOfAttack, tiltAngle},
+              output: {location: rotatedGrounded, time: groundedTime},
             });
           }
         }
       }
     }
-    this.samples.sort((a, b) => a.output[0][0] - b.output[0][0]);
+    this.samples.sort((a, b) => a.output.location[0] - b.output.location[0]);
     console.log('Range finder ready. maxDistance = ' + this.getMaxDistance());
   }
 
   getMaxDistance() {
-    return this.samples[this.samples.length - 1].output[0][0];
+    return this.samples[this.samples.length - 1].output.location[0];
   }
 
   // TODO: Fix this code. It currently assumes that all output values are
   // [x, 0, 0], which is NOT true due to tiltAngle.
   getBestSample(distance, minTime) {
-    let filteredSamples = minTime ? this.samples.filter(s => s.output[1] > minTime) : this.samples;
+    let filteredSamples = minTime ? this.samples.filter(s => s.output.time > minTime) : this.samples;
     if (filteredSamples.length === 0) { return null; }
     // Binary search over filteredSamples to find the nearest sample throw
     let min = 0;
     let max = filteredSamples.length - 1;
-    if (filteredSamples[min].output[0][0] > distance || filteredSamples[max].output[0][0] < distance) {
+    if (filteredSamples[min].output.location[0] > distance || filteredSamples[max].output.location[0] < distance) {
       return null;
     }
     while (max - min > 1) {
       let mid = Math.trunc((min + max) / 2);
-      if (filteredSamples[mid].output[0][0] > distance) {
+      if (filteredSamples[mid].output.location[0] > distance) {
         max = mid;
       } else {
         min = mid;
@@ -83,15 +88,17 @@ export class RangeFinder {
   getThrowParams(vector2d, minTime) {
     const sample = this.getBestSample(mag2d(vector2d), minTime);
     if (!sample) { return null; }
-    const [forward, upward, angleOfAttack, tiltAngle] = sample.input;
-    const velocity = mul2d(vector2d, forward / mag2d(vector2d)).concat(upward);
-    return [velocity, angleOfAttack, tiltAngle];
+    const {velocity, angleOfAttack, tiltAngle} = sample.input;
+    const vectorAngle = Math.atan2(vector2d[1], vector2d[0]);
+    const rotatedVelocity = rotate2d(velocity, vectorAngle).concat(velocity[2]);
+    return [rotatedVelocity, angleOfAttack, tiltAngle];
   }
 
   // returns [velocity, angleOfAttack, tiltAngle]
   getLongestThrowParams(vector2d) {
-    const [forward, upward, angleOfAttack, tiltAngle] = this.samples[this.samples.length - 1].input;
-    const velocity = mul2d(vector2d, forward / mag2d(vector2d)).concat(upward);
-    return [velocity, angleOfAttack, tiltAngle];
+    const {velocity, angleOfAttack, tiltAngle} = this.samples[this.samples.length - 1].input;
+    const vectorAngle = Math.atan2(vector2d[1], vector2d[0]);
+    const rotatedVelocity = rotate2d(velocity, vectorAngle).concat(velocity[2]);
+    return [rotatedVelocity, angleOfAttack, tiltAngle];
   }
 }
