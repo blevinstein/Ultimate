b509bdf5199ded8800872f25ab6bfb5feafc6638
Brian Levinstein <blevinstein@gmail.com>
2020-05-02 17:51:59 -0700

    Headless runs 100 games of zone vs man

--- src/headless.js
+++ src/headless.js
@@ -1,4 +1,7 @@
 const {
+    Coach
+} = require('./coach.js');
+const {
     Game
 } = require('./game.js');
 const {
@@ -7,9 +10,29 @@ const {
 const {
     ConsoleToastService
 } = require('./console_toast_service.js');
+const {
+    ZoneDefenseStrategy
+} = require('./strategy/zone_defense.js');
 
-const game = new Game().setToastService(new ConsoleToastService());
+const game = new Game(null, null, [new Coach(), new Coach(undefined, (game, team) => new ZoneDefenseStrategy(game, team))]);
 
-while (game.state != STATES.GameOver) {
-    game.update();
+
+let zoneWins = 0;
+let manWins = 0;
+for (let i = 0; i < 100; i++) {
+    game.reset();
+    while (game.state != STATES.GameOver) {
+        game.update();
+    }
+    if (game.teams[0].score === game.teams[1].score) {
+        throw new Error('Tie game should never happen!');
+    }
+    if (game.teams[0].score > game.teams[1].score) {
+        ++manWins;
+    } else {
+        ++zoneWins;
+    }
+    console.log('.');
 }
+
+console.log(`Results: man-to-man ${manWins} zone ${zoneWins}`);
