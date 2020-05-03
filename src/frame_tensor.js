67a901ba104f8372e4a8e73b270de32e69deaacc
Brian Levinstein <blevinstein@gmail.com>
2020-05-02 19:27:16 -0700

    Add frame_tensor and record_games for recording game data for training.

---
+++ src/frame_tensor.js
@@ -0,0 +1,129 @@
+const fs = require('fs');
+const stringify = require('csv-stringify');
+
+const {
+  STATES
+} = require('./game_params.js');
+
+module.exports.FrameTensor = class FrameTensor {
+  constructor() {
+    this.headers = this.allKeys();
+    this.headerSet = new Set(this.headers);
+
+    this.frames = [];
+
+    this.frameValues = new Map;
+  }
+
+  allKeys() {
+    const keys = ['state', 'stallCount', 'disc_x', 'disc_y', 'disc_z'];
+    for (let t = 0; t < 2; t++) {
+      for (let p = 0; p < 7; p++) {
+        keys.push(
+          `team_${t}_player_${p}_x`,
+          `team_${t}_player_${p}_y`,
+          `team_${t}_player_${p}_vx`,
+          `team_${t}_player_${p}_vy`,
+          `team_${t}_player_${p}_action`,
+          `team_${t}_player_${p}_move_x`,
+          `team_${t}_player_${p}_move_y`,
+          `team_${t}_player_${p}_throw_x`,
+          `team_${t}_player_${p}_throw_y`,
+          `team_${t}_player_${p}_throw_z`,
+          `team_${t}_player_${p}_throw_angleOfAttack`,
+          `team_${t}_player_${p}_throw_tiltAngle`,
+        );
+      }
+    }
+    return keys;
+  }
+
+  record(key, value) {
+    if (this.frameValues.has(key)) {
+      throw new Error(`Duplicate key: ${key}`);
+    }
+    if (!this.headerSet.has(key)) {
+      throw new Error(`Unexpected key: ${key}`);
+    }
+    this.frameValues.set(key, value);
+  }
+
+  recordInitialState(game) {
+    this.record('state', game.state);
+    this.record('stallCount', game.stallCount);
+    for (let t = 0; t < game.teams.length; ++t) {
+      for (let p = 0; p < game.teams[t].players.length; ++p) {
+        const player = game.teams[t].players[p];
+        this.record(`team_${t}_player_${p}_x`, player.position[0]);
+        this.record(`team_${t}_player_${p}_y`, player.position[1]);
+        this.record(`team_${t}_player_${p}_vx`, player.velocity[0]);
+        this.record(`team_${t}_player_${p}_vy`, player.velocity[1]);
+      }
+    }
+    this.record('disc_x', game.disc.position[0]);
+    this.record('disc_y', game.disc.position[1]);
+    this.record('disc_z', game.disc.position[2]);
+  }
+
+  recordActions(game, actionMap) {
+    for (let t = 0; t < game.teams.length; ++t) {
+      for (let p = 0; p < game.teams[t].players.length; p++) {
+        const player = game.teams[t].players[p];
+        if (!actionMap.has(player)) {
+          this.record(`team_${t}_player_${p}_action`, 'rest');
+        } else {
+          const [action, detail] = actionMap.get(player);
+          this.record(`team_${t}_player_${p}_action`, action);
+          if (action === 'move') {
+            this.record(`team_${t}_player_${p}_move_x`, detail[0]);
+            this.record(`team_${t}_player_${p}_move_y`, detail[1]);
+          } else {
+            const [velocity, angleOfAttack, tiltAngle] = detail;
+            this.record(`team_${t}_player_${p}_throw_x`, velocity[0]);
+            this.record(`team_${t}_player_${p}_throw_y`, velocity[1]);
+            this.record(`team_${t}_player_${p}_throw_z`, velocity[2]);
+            this.record(`team_${t}_player_${p}_throw_angleOfAttack`, angleOfAttack);
+            this.record(`team_${t}_player_${p}_throw_tiltAngle`, tiltAngle);
+          }
+        }
+      }
+    }
+  }
+
+  nextFrame() {
+    const state = this.frameValues.get('state');
+    if (state == STATES.Pickup || state == STATES.Normal || state == STATES.Receiving) {
+      // Only save data from 'interesting' frames.
+      this.frames.push(this.frameValues);
+    }
+    this.frameValues = new Map;
+  }
+
+  dumpToFile(filename) {
+    const lines = [];
+    const stringifier = stringify({
+      delimiter: ','
+    });
+    stringifier.on('readable', () => {
+      let row;
+      while (row = stringifier.read()) {
+        lines.push(row);
+      }
+    });
+    stringifier.on('error', (e) => {
+      console.error(e.message);
+    });
+    stringifier.on('finish', () => {
+      fs.writeFile(filename, lines.join(''), (e) => {
+        if (e) throw err;
+        console.log(`Wrote ${this.frames.length} frames to ${filename}`);
+      });
+    });
+    stringifier.write(this.headers);
+    for (let i = 0; i < this.frames.length; i++) {
+      stringifier.write(this.headers.map(h => this.frames[i].has(h) ? this.frames[i].get(h) :
+        ''));
+    }
+    stringifier.end();
+  }
+};
---
+++ src/record_games.js
@@ -0,0 +1,61 @@
+const flags = require('flags')
+
+const {
+  Coach
+} = require('./coach.js');
+const {
+  Game
+} = require('./game.js');
+const {
+  STATES
+} = require('./game_params.js');
+const {
+  ConsoleToastService
+} = require('./console_toast_service.js');
+const {
+  FrameTensor
+} = require('./frame_tensor.js');
+const {
+  ZoneDefenseStrategy
+} = require('./strategy/zone_defense.js');
+
+flags.defineInteger('games', 10, 'Number of games to simulate');
+flags.defineString('output', 'output.csv', 'File to store result in CSV format');
+flags.parse();
+
+let frameTensor = new FrameTensor();
+for (let i = 0; i < flags.get('games'); ++i) {
+  const game = new Game(null, null, [
+    new Coach(),
+    new Coach(undefined, (game, team) => new ZoneDefenseStrategy(game, team))
+  ]);
+  const actionMap = new Map;
+
+  game.onNewStrategy = strategy => {
+    strategy.onMove = (player, destination) => {
+      if (actionMap.has(player)) {
+        throw new Error('Player already has an action registered!');
+      }
+      actionMap.set(player, ['move', destination]);
+    };
+    strategy.onThrow = (player, params) => {
+      if (actionMap.has(player)) {
+        throw new Error('Player already has an action registered!');
+      }
+      actionMap.set(player, ['throw', params]);
+    }
+  };
+
+  let steps = 0;
+  while (game.state != STATES.GameOver) {
+    ++steps;
+    frameTensor.recordInitialState(game);
+    game.update();
+    frameTensor.recordActions(game, actionMap);
+    frameTensor.nextFrame();
+    actionMap.clear();
+  }
+  console.log(`Game over! ${steps} steps`);
+}
+
+frameTensor.dumpToFile(flags.get('output'));
