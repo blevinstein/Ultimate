442d6bb811a9b5fb7f802e746bc6360d7a157cde
Brian Levinstein <blevinstein@gmail.com>
2020-05-06 22:04:11 -0700

    fmt

--- src/player.js
+++ src/player.js
@@ -241,7 +241,7 @@ module.exports.Player = class Player {
       velocity = mul3d(norm3d(velocity), MAX_THROW_SPEED);
       console.log(
         `Tried to throw too fast: ${mag3d(velocity)} > ${MAX_THROW_SPEED}`
-        );
+      );
     }
     if (!this.hasDisc) {
       console.log('Attempted to throw without the disc!');
