d939486485cfa312225bd3312b10529990ec53a3
Brian Levinstein <blevinstein@gmail.com>
2020-06-14 19:20:51 -0700

    tweak

--- src/record_games.js
+++ src/record_games.js
@@ -44,7 +44,7 @@ const ACTION_COLUMNS = [
   'team_1_player_5_action',
   'team_1_player_6_action',
 ];
-const MAX_IN_MEMORY_SAMPLES = 500e3;
+const MAX_IN_MEMORY_SAMPLES = 200e3;
 
 // Play a single game until completion, and return as a FrameTensor.
 function playGame() {
