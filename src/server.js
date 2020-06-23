4e24f5dcaf0ac9333c44944230ecddee3b71f448
Brian Levinstein <blevinstein@gmail.com>
2020-06-23 01:40:01 -0700

    Add flag for disabling live js reloading

--- src/server.js
+++ src/server.js
@@ -4,23 +4,28 @@ const flags = require('flags')
 const path = require('path');
 
 flags.defineInteger('port', 8000, 'Port for serving over http');
+flags.defineBoolean('live', true,
+  'True for live reloading, false for compiled build');
 flags.parse();
 
 const app = express()
 const port = flags.get('port');
-const browserifyOptions = {
-  watch: true,
-  precompile: true,
-};
 
 app.use(express.static('static/'));
 app.use(express.static('js_model/'));
 
-for (let root of ['index.js', 'play.js', 'practice_main.js']) {
-  app.get(
-    path.join('/', root),
-    expressBrowserify([path.join('src', root)],
-      browserifyOptions));
+if (flags.get('live')) {
+  for (let root of ['index.js', 'play.js', 'practice_main.js']) {
+    app.get(
+      path.join('/', root),
+      expressBrowserify([path.join('src', root)], {
+        watch: true,
+        precompile: true
+      }));
+  }
+} else {
+  console.log('Using static build');
+  app.use(express.static('build/'));
 }
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
