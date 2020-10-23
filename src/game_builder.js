aa556bc106db1da37707131215c721db642dc7e2
Brian Levinstein <blevinstein@gmail.com>
2020-10-22 23:07:23 -0700

    Add plan for GameBuilder to de-couple model deps from frontend setup

---
+++ src/game_builder.js
@@ -0,0 +1,24 @@
+// GameBuilder
+//
+// async load resources, models, populations as necessary
+// initialized state (boolean)
+// build coaching strategies from config
+// mapping from string name to builder function
+//
+// TODO: Support tensorflow and pytorch?
+//
+// 'static': {
+//   'offense': 'Strategy',
+//   'defense': 'Strategy',
+//   ['aerial': 'Strategy',]
+//  }
+//  'models': [
+//    {'path': 'model_1.json'},
+//    ...
+//  ]
+//  'model_population: {
+//    'path': 'population_path/'
+//  }
+//  'double_model_population: {
+//    'path': 'population_path/'
+//  }
